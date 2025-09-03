705a7f003c48a6b3f739153159dcf10081b5fb94
vamundse <vamundse@gmail.com>
2025-09-03 09:29:56 +0200

    Changed the load script to defer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,5 +4,4 @@ const lastModified = document.querySelector("#lastModified");
 const today = new Date();
 
 lastModified.innerHTML = `Last modified: ${document.lastModified}`;
-
 currentyear.innerHTML = `${today.getFullYear()}`;
