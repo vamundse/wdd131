b16de7739132d2969b678099913f37cb3132c693
vamundse <vamundse@gmail.com>
2025-09-03 09:23:32 +0200

    Changed the js to show in text not as a pop up

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,6 +3,6 @@ const lastModified = document.querySelector("#lastModified");
 
 const today = new Date();
 
-lastModified.innerHTML = `Last modified: ${alert(document.lastModified)}`;
+lastModified.innerHTML = `Last modified: ${document.lastModified}`;
 
 currentyear.innerHTML = `${today.getFullYear()}`;
