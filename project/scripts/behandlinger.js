587c6da268ac24bb25adf64f2c310491e114b1c1
vamundse <vamundse@gmail.com>
2025-10-09 16:37:07 +0200

    A lot

---
+++ project/scripts/behandlinger.js
@@ -0,0 +1,39 @@
+const ansigt = document.querySelector("#ansigtbrug");
+const facelift = document.querySelector("#faceliftbrug");
+
+let ansigtzone = [
+    "Sclerose",
+    "Migræne",
+    "Smerter i led og muskler",
+    "Stress, kredsløbsbesvær",
+    "Søvnproblemer, fordøjelsesproblemer",
+    "Allergi, eksem og psoriasis",
+    "Hovedpine",
+    "Diabetes",
+    "Følger efter hjernerystelse",
+    "Koncentrationsbesvær",
+    "Infertilitet",
+    "Gener ved overgangsalder"  
+];
+
+let facelifting = [
+    "Forbedre blodcirkulationen",
+    "Understøtte udskillelse af toksiner",
+    "Regenerere væv",
+    "Accelerere cellefornyelsen",
+    "Formindske rynker og slap hud",
+    "Reducere alderspletter",
+    "Afhjælpe muskelspændinger",
+    "Øge hudens spændstighed"
+];
+
+ansigtzone.forEach(perk => {
+    ansigt.innerHTML += `<p>${perk}</p> `;
+});
+
+facelifting.forEach(perk => {
+    facelift.innerHTML += `<p>${perk}</p>`;
+});
+
+
+
---
+++ project/scripts/timedate.js
@@ -0,0 +1,9 @@
+const opdateret = document.querySelector("#opdateret");
+const year = document.querySelector("#year");
+
+const today = new Date();
+const lastmod = new Date(document.lastModified);
+
+
+opdateret.innerHTML = `Sist opdateret: ${lastmod.getDate()}/${lastmod.getMonth()+1}/${lastmod.getFullYear()}`;
+year.innerHTML = `Copyright &copy; ${today.getFullYear()}, Christina Gade.`;
